33390d3c60226ca38e6552e59a1b8808eda28889
Thikamporn Simud <thikamporn.s@ku.th>
2019-07-11 09:22:23 +0700

    add procfile

--- index.js
+++ index.js
@@ -7,52 +7,58 @@
  
 // app.listen(3000)
 
-'use strict';
+// 'use strict';
 
-const line = require('@line/bot-sdk');
-const express = require('express');
+// const line = require('@line/bot-sdk');
+// const express = require('express');
 
-// create LINE SDK config from env variables
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
+// // create LINE SDK config from env variables
+// const config = {
+//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+//   channelSecret: process.env.CHANNEL_SECRET,
+// };
 
-// create LINE SDK client
-const client = new line.Client(config);
+// // create LINE SDK client
+// const client = new line.Client(config);
 
-// create Express app
-// about Express itself: https://expressjs.com/
-const app = express();
+// // create Express app
+// // about Express itself: https://expressjs.com/
+// const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
+// // register a webhook handler with middleware
+// // about the middleware, please refer to doc
+// app.post('/webhook', line.middleware(config), (req, res) => {
+//   Promise
+//     .all(req.body.events.map(handleEvent))
+//     .then((result) => res.json(result))
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).end();
+//     });
+// });
 
-// event handler
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
+// // event handler
+// function handleEvent(event) {
+//   if (event.type !== 'message' || event.message.type !== 'text') {
+//     // ignore non-text-message event
+//     return Promise.resolve(null);
+//   }
 
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+//   // create a echoing text message
+//   const echo = { type: 'text', text: event.message.text };
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-}
+//   // use reply API
+//   return client.replyMessage(event.replyToken, echo);
+// }
 
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
+// // listen on port
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`listening on ${port}`);
+// });
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+app.post('/webhook', (req, res) => res.sendStatus(200))
+app.listen(port)
