3211bb4c0b7b6ded73197b641104adc943f7bb71
Thikamporn Simud <thikamporn.s@ku.th>
2019-07-11 11:15:05 +0700

    edit reqiure webhookHandler

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const config = require('./config.js');
-const webhookHandler = require('./handler/webhook');
+const webhookHandler = require('./handlers/webhookHandler.js');
 
 // listen on port
 const port = process.env.PORT || 4000
